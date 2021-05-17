8b1079d1e4ff9f03695e2d78c846c1cb9c7f6d11
Aleksandar Ananiev <aleksandar.ananiev@mobilewaves.com>
2021-05-17 22:12:15 +0300

    react app

--- zip-build.js
+++
@@ -1,6 +0,0 @@
-let zip = require("./simpleZip");
-let config = require('./package.json');
-
-(async () => {
-  await zip("./releases/"+config.gameFolder+"_"+config.version, "./releases/"+config.gameFolder+"_"+config.version+".zip");
-})();
