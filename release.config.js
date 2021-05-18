e47d00b53c6ff0e0fa713b1a9ef9857c5201409c
Aleksandar Ananiev <aleksandar.ananiev@mobilewaves.com>
2021-05-18 22:32:11 +0300

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryURL: "https://github.com/aaananiev/test-game-workflow",
+    plugins: [
+            '@semantic-release/commit-analyzer',
+            '@semantic-release/release-notes-generator',
+            '@semantic-release/github'
+            ]
+}
