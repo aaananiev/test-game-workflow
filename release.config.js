2a4872f94d1bd68675f0f7ac91ba150a96e34eeb
Aleksandar Ananiev <aleksandar.ananiev@mobilewaves.com>
2021-05-18 22:43:34 +0300

    style: code formating

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryURL: "https://github.com/aaananiev/test-game-workflow",
-    plugins: [
-            '@semantic-release/commit-analyzer',
-            '@semantic-release/release-notes-generator',
-            '@semantic-release/github'
-            ]
-}
+  branches: "master",
+  repositoryURL: "https://github.com/aaananiev/test-game-workflow",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
